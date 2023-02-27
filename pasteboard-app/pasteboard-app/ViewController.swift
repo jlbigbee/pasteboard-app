1fccfe0506848661340703b5f29856e2080b1530
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-27 15:12:45 -0800

    connected UI elements to code

--- pasteboard-app/pasteboard-app/ViewController.swift
+++ pasteboard-app/pasteboard-app/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textView.text = "Hello"
     }
 
 
