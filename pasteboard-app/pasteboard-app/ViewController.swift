80f3a44e04e2a1153b706cb26a9ab524e30e3204
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-27 14:59:58 -0800

    created basic layouts with storyboards

---
+++ pasteboard-app/pasteboard-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pasteboard-app
+//
+//  Created by Jonathan Bigbee on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
